aca3fa4f9de6a5136f8c920d5926eda610ef4e6d
Antonio Martinez <antoniomartinez12347@gmail.com>
2023-06-04 23:17:40 -0600

    N to M relationship was implemented.

---
+++ app/src/main/java/com/example/android/mutipleroomtables/data/entities/relations/StudentSubjectCrossRef.kt
@@ -0,0 +1,9 @@
+package com.example.android.mutipleroomtables.data.entities.relations
+
+import androidx.room.Entity
+
+@Entity(primaryKeys = ["studentName","subjectName"])
+data class StudentSubjectCrossRef(
+    val studentName: String,
+    val subjectName: String
+)
